7bf37b2a393c0ab7f4aa4c34edf9b834cf584128
lokesh1kush <imlokeshji@gmail.com>
2025-09-13 22:54:12 +0530

    Your commit message

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+// Premitive dataTypes ----------
+// 1. number => 1,2,3....
+// 2. string => ''
+// 3. boolean => true/false
+// 4. null => set empty by programmer  and type of null is object, that is bug of javaScript 
+// 5. undefined => not defined
+// 6. symbol => unique
+// 7. bigint => used for larger numbers
+
+
+// non Premitive dataTypes ---------
+// 1. object => key and Value pair
+// 2. array => list of values
+// 3. function => block of reusable code
+
---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,46 @@
+// let score = 33;
+// let convertScore = String(score);
+// console.log(typeof convertScore);
+// console.log(convertScore);
+
+//Number => String..........
+
+
+// let health = '33';
+// let convertHealth = Number(health)
+// console.log(typeof convertHealth);
+// console.log(convertHealth);
+
+// String => Number...........
+
+
+// let yoyo = '33abc';
+// let convertYoyo = Number(yoyo);
+// console.log(typeof convertYoyo);
+// console.log(convertYoyo);
+
+// type String => Number but if you print that it is NaN
+
+
+
+// let bull = true;
+// let convertbull = Number(bull);
+// console.log(typeof convertbull);
+// console.log(convertbull);
+
+//Boolean => Number (true => 1,  false => 0)
+
+
+// let num = 10;
+// let converNum = Boolean(num);
+// console.log(typeof converNum);
+// console.log(converNum);
+
+//Number => Boolean (0 => false,  any number positive or nagetive => true)
+
+
+
+let empty = null;
+let convertEmpty = Number(empty);
+console.log(typeof convertEmpty);
+console.log(convertEmpty);
