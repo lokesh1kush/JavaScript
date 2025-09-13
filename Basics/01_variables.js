7e2750e068ea2442aec7a828e84e1fa6214c6ccf
lokesh1kush <imlokeshji@gmail.com>
2025-09-13 15:20:15 +0530

    Your commit message

---
+++ Basics/01_variables.js
@@ -0,0 +1,14 @@
+var name = 'Lokesh';
+const age = 24;
+let city = 'jaipur';
+
+console.log(name);
+console.log(age);
+console.log(city);
+
+console.table([name, age, city]);
+
+/*
+prefer not to use 'var' variable
+because of issue of block scope and functional scope
+*/
